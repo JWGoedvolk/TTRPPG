9b8d57afeee4d547752093dbbd75a27f1001c1d3
Jan Willem Goedvolk <jan.goedvolk@student.saedubai.com>
2024-06-19 11:58:08 +0400

    Basic Roller
    Made a scriptable object for rolling dice using "xdy" format: x = number of dice, y = number of sides on the dice.

--- Assets/Scripts/Dice/DiceRoller.cs
+++ Assets/Scripts/Dice/DiceRoller.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using JW.TTRPG.ScriptableObjects;
 
-public class DiceRoller : MonoBehaviour
+namespace JW.TTRPG.Utility
 {
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(fileName = "Roller", menuName = "JW/ScriptableObjects/Utilities/DiceRoller")]
+    public class DiceRoller : ScriptableObject
     {
-        
-    }
+        #region Variables
+        [SerializeField] private ScriptableString equation;
+        [SerializeField] private ScriptableInt result;
+        #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
+        #region Public Functions
+        public void Roll(ScriptableString formula)
+        {
+            Debug.Log($"Rolling {formula.Value}");
+            var toRoll = formula.Value.Split('d');
+
+            int numDice = int.Parse(toRoll[0]);
+            int diceSize = int.Parse(toRoll[1]);
+
+            int total = 0;
+            string rolls = "";
+            for (int roll = 0; roll < numDice; roll++)
+            {
+                rolls += $"  {roll}: ";
+                int rolled = Random.Range(1, diceSize + 1);
+                rolls += $"{rolled}\n";
+                total += rolled;
+            }
+
+            Debug.Log(rolls);
+            Debug.Log($"Result: {total}");
+            result.Value = total;
+        }
+        #endregion
+
+        #region Private Functions
+
+        #endregion
+
+        #region Unity Specific Functions
         
-    }
+        #endregion
+    } 
 }
